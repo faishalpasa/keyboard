10ccf6e8b0811f5fd62f3c60a109c9836334e0b4
Pasa <mfaishal_pasa@hotmail.com>
2021-12-18 07:34:24 +0700

    [FIX] color on keyboard

--- src/Keyboard/KeyboardContainer.js
+++ src/Keyboard/KeyboardContainer.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import KeyboardView from './KeyboardView';
 
-const colorList = ['white', 'red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+const colorList = ['white', 'red', 'orange', 'yellow', 'green', 'blue', 'turquoise', 'purple'];
 
 const KeyboardContainer = () => {
   const [isCapslockActive, setIsCapslockActive] = useState(false);
@@ -10,7 +10,7 @@ const KeyboardContainer = () => {
 
   const handleChangeKeyboardColor = () => {
     const colorIndex = colorList.indexOf(color)
-    if (colorIndex >= 6) {
+    if (colorIndex >= 7) {
       setColor(colorList[0])
     } else {
       setColor(colorList[colorIndex + 1])
