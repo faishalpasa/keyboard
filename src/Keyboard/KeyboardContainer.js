fb7cb289e67e4b2ac37ca70e204c8a2dbf2dd1c5
Pasa <mfaishal_pasa@hotmail.com>
2021-12-22 12:16:57 +0700

    [FIX] missing digit0 in acceptdet key code

--- src/Keyboard/KeyboardContainer.js
+++ src/Keyboard/KeyboardContainer.js
@@ -4,7 +4,7 @@ import KeyboardView from './KeyboardView';
 
 const COLOR_LIST = ['white', 'red', 'orange', 'yellow', 'green', 'blue', 'turquoise', 'purple'];
 const ACCEPTED_KEY_CODES = [
-  'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Minus', 'Equal',
+  'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0','Minus', 'Equal',
   'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash',
   'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote',
   'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash',
